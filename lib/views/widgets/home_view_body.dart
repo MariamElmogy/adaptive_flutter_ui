1a16119a8506cbc4b165370cf6156c74126c5895
mariam <mariamelmogy3@gmail.com>
2024-02-04 20:44:51 +0200

    - refactor home_view - create adaptiveLayout

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_ui_project/views/widgets/adaptive_layout.dart';
 import 'package:responsive_ui_project/views/widgets/desktop_layout.dart';
 import 'mobile_layout.dart';
 import 'tablet_layout.dart';
@@ -8,16 +9,10 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth < 600) {
-          return const MobileLayout();
-        } else if (constraints.maxWidth < 900) {
-          return const TabletLayout();
-        } else {
-          return const DesktopLayout();
-        }
-      },
+    return AdaptiveLayout(
+      mobileLayout: (context) => const MobileLayout(),
+      tabletLayout: (context) => const TabletLayout(),
+      desktopLayout: (context) => const DesktopLayout(),
     );
   }
 }
